90d17f8e1c6afc91e8a2b312ed71cd48ecd5e257
mohitdmak <mdmohitrc@gmail.com>
2022-03-28 08:08:54 +0530

    chk

--- src/connection.py
+++ src/connection.py
@@ -12,25 +12,33 @@ DB = MySQLdb.connect(
 
 # Main database cursor class
 class CURSOR:
+    # Instantiate a DB cursor for every CURSOR instance
     def __init__(self):
         self.DB_CURSOR = DB.cursor()
 
+    # Let function decide what to extract from res
     def return_results(self):
         return self.DB_CURSOR.fetchall()
 
+    # Print all res output from mysql db
     def print_results(self):
         for row in self.DB_CURSOR.fetchall():
             print(row)
 
+    # Execute input cmd at current DB CURSOR
     def execute_command(self, cmd):
         self.DB_CURSOR.execute(cmd)
         
 # Erp database cursor instance
 ERP_DB = CURSOR()
+
+# Test executions ---------------------------
 ERP_DB.execute_command("SHOW TABLES")
 res = ERP_DB.return_results()
 for x in res:
     ERP_DB.execute_command(f"DESCRIBE {x[0]}")
     ERP_DB.print_results()
+# --------------- ---------------------------
 
+# End connection
 DB.close()
