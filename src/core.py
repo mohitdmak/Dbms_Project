0954723ea9410eee5e3f89b8d52f61a8df1e4a01
mohitdmak <mdmohitrc@gmail.com>
2022-03-28 10:39:43 +0530

    add: update handler

--- src/core.py
+++ src/core.py
@@ -1,9 +1,24 @@
 from connection import *
 
 # Test executions ---------------------------
-ERP_DB.execute_command("SHOW TABLES;")
-ERP_DB.print_results()
+# ERP_DB.execute_command("SHOW TABLES;")
+# ERP_DB.print_results()
 # --------------- ---------------------------
+
+# Class handling updations to db
+class UpdateIn():
+    def genByConds(self, table, col, val, **kwargs):
+        """:param1 table :params Variable no of key-value pair conditions"""
+        condition = f"UPDATE {table} SET {col} = {val} WHERE "
+        cond_keys = list(kwargs.keys())
+        last = cond_keys.pop(len(cond_keys) - 1) if len(cond_keys) > 0 else None
+        for x in cond_keys:
+            condition += f"{x} = {kwargs[x]} AND "
+        if last != None:
+            condition += f"{last} = {kwargs[str(last)]};"
+        else:
+            raise ProgERROR(f"Attempting to Update Table {table}!")
+        ERP_DB.execute_command(condition)
 
 # Class handling deletions to db
 class DeleteFrom():
@@ -40,19 +55,20 @@ class InsertIn():
             f'INSERT INTO teacher(name) VALUES ("{name}");'   
         )
 
-    def course(self, name, IC_id, capacity = 50) -> None:
+    def course(self, name, IC_id = "NULL", capacity = 50) -> None:
         """:param1 name :param2 IC_id :param3(Optional) capacity"""
         # Check if a teacher of IC_id exists raise error if not
-        ERP_DB.execute_command( f'SELECT * FROM teacher where id = {IC_id};')
-        if len(ERP_DB.return_results()) == 0:
-            raise ProgERROR(f"No IC. Teacher of id {IC_id} exists!")
+        if IC_id != "NULL":
+            ERP_DB.execute_command( f'SELECT * FROM teacher where id = {IC_id};')
+            if len(ERP_DB.return_results()) == 0:
+                raise ProgERROR(f"No IC. Teacher of id {IC_id} exists!")
         # Check value limits of name and capacity
         if len(name) > 20:
             raise ProgERROR(f"Name(course) crosses len limit 20: {name}")
         if capacity > 99:
             raise ProgERROR(f"Capacity(course) crosses limit 99: {capacity}")
         ERP_DB.execute_command(
-            f'INSERT INTO course(name, capacity, IC_id) VALUES ("{name}", "{capacity}", "{IC_id}");'
+            f'INSERT INTO course(name, capacity, IC_id) VALUES ("{name}", "{capacity}", {IC_id});'
         )
 
     def assists(self, teacher_id, course_id):
@@ -125,12 +141,16 @@ class InsertIn():
 
 
 # Test executions ---------------------------
-InsertHandler = InsertIn()
-InsertHandler.course("Test", 1)
-InsertHandler.addn_course(1, 3)
-DeleteHandler = DeleteFrom()
-DeleteHandler.genByConds("course", id = 2)
+# InsertHandler = InsertIn()
+# InsertHandler.course("Tst")
+# InsertHandler.course("Tst", 1)
+# InsertHandler.addn_course(1, 3)
+# DeleteHandler = DeleteFrom()
+# DeleteHandler.genByConds("course", id = 2)
 # DeleteHandler.genByConds("add_course", student_id = 1, course_id = 2)
+# UpdateHandler = UpdateIn()
+# UpdateHandler.genByConds("course", "name", "'check'", id = 1, capacity = 20)
+# UpdateHandler.genByConds("course", "name", "'check'", name = "'newtest'")
 # --------------- ---------------------------
 
 ERP_DB.exit()
