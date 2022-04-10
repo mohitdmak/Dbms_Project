ed1af15daee3d0b793d1946da8abdc8674b5582f
Kathan0 <f20200058@pilani.bits-pilani.ac.in>
2022-04-10 16:16:56 +0530

    Updated html files

--- client/test.js
+++ client/test.js
@@ -4,7 +4,6 @@ function onSubmit(){
     
 const username = document.getElementById("Username").value;
 const password = document.getElementById("Password").value;
-document.getElementById("Username").value = "";
 document.getElementById("Password").value = "";
     
 
@@ -24,6 +23,7 @@ document.getElementById("Password").value = "";
 
           if(parseInt(res.data.id) > 0){
             console.log(res.data);
+            localStorage.setItem("Name", username);
             localStorage.setItem("ID", res.data.id);
             console.log(localStorage);
             location.href = 'http://127.0.0.1:5500/client/course.html'
